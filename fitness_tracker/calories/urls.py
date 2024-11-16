899d843dbd4d208f5eec26aca0d631df2b361176
RishiNCI <x23303891@student.ncirl.ie>
2024-11-16 21:06:03 +0000

    Delete fitness_tracker directory

--- fitness_tracker/calories/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('meal_log/', views.meal_log, name='meal_log'),
-]
--- fitness_tracker/gym/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Workout
-from .models import Profile
-
-
-admin.site.register(Workout)
-admin.site.register(Profile)
-
-
