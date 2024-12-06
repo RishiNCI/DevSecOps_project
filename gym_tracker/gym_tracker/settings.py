ff26907defc65f71811e2e5fcd30fe1a65f4a59d
RishiNCI <x23303891@student.ncirl.ie>
2024-12-06 00:57:43 +0000

    req.txt

--- gym_tracker/gym_tracker/settings.py
+++ gym_tracker/gym_tracker/settings.py
@@ -100,6 +100,8 @@ USE_TZ = True
 # Static files
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default auto field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
